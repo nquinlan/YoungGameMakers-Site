2e4e2846ba7b34bd1eaf59644c1b407be5e0cdb0
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-08-15 19:47:57 -0400

    ok this is messed. addTo not working

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,11 +19,6 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'stacey.mulcahy@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "YOUNG GAME MAKERS:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-
 
 $email = new SendGrid\Email();
 
@@ -32,6 +27,7 @@ $email->addTo('stacey.mulcahy@gmail.com')->
        setSubject('[Young Makers Inquiry]')->
        setText('stuff things');
 var_dump($email);
+
 $response = $sendgrid->send($email);
 var_dump($response);
 ?>
