b4c6c1d86622ee14c131f5b45914ed990ee3217c
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-08-15 19:44:19 -0400

    hardcoded email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -27,7 +27,7 @@ $email_body = "You have received a new message from your website contact form.\n
 
 $email = new SendGrid\Email();
 
-$email->addTo($to)->
+$email->addTo('stacey.mulcahy@gmail.com')->
          setFrom('stacey.mulcahy@gmail.com')->
        setSubject('[Young Makers Inquiry]')->
        setText('stuff things');
